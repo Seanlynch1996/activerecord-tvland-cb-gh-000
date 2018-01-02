5dfa7279534a23f4ea48213747214a3c32ed9e52
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2018-01-02 22:14:27 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,9 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+  belongs_to :network
 
-  def build_network(val)
+  def build_network(options = {})
+    
   end
 end
